9ee1efe40f031b3cdd94489810bd5013ffba6b50
jumbriaco5 <jumbriaco@student.cscc.edu>
2024-11-11 11:44:29 -0500

    Added V1 of Aim Mechanic
    The aiming isn't very accurate and it  needs more work, but it is a start to the mechanic. I'll work on it some more soon.

--- TargetAcquisition.js
+++ TargetAcquisition.js
@@ -9,6 +9,9 @@ context = canvas.getContext('2d'),
 background = new Image(), //background image of canvas, not the webpage. The webpage background is done in the CSS file
 playerImage = new Image();
 
+mouseX = canvas.width / 2,
+mouseY = canvas.height / 2;
+
 initializeImages();
 
 function debug(stringText) //updates the debug element with a value given
@@ -27,25 +30,42 @@ function initializeImages() //pretty much ripped from snailbait
       //debug("yes");
    };
 
-   function drawGame()
-   {
-      //Level ----------
-      context.drawImage(background, 0, 0); //background drawn
+   //Track mouse position
+   canvas.addEventListener('mousemove', function(event) {
+      const rect = canvas.getBoundingClientRect();
+      mouseX = event.clientX - rect.left;
+      mouseY = event.clientY - rect.top;
+      drawGame(); // Redraw game each time the mouse moves
+   });
 
-      //Player ----------
-
-      //Scale the player image down
+   function drawGame() {
+      // Draw background
+      context.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
+      context.drawImage(background, 0, 0, canvas.width, canvas.height);
+   
+      // Scale player image down
       const playerImageScale = 0.2;
       const playerWidth = playerImage.width * playerImageScale;
       const playerHeight = playerImage.height * playerImageScale;
-
-      // Center the player image on the canvas
+   
+      // Position player in the bottom-left corner
       const playerX = 0;
-      const playerY = ((canvas.height - playerHeight) / 2) + 300;
+      const playerY = canvas.height - playerHeight;
+   
+      // Calculate rotation angle
+      const angle = calculateAngleToMouse(playerX + playerWidth / 2, playerY + playerHeight / 2);
+   
+      // Rotate and draw player image
+      context.save();
+      context.translate(playerX + playerWidth / 2, playerY + playerHeight / 2)
+      context.rotate(angle);
+      context.drawImage(playerImage, -playerWidth / 2, -playerHeight / 2, playerWidth, playerHeight);
+      context.restore();
+   }
 
-      context.drawImage(playerImage, playerX, playerY, playerWidth, playerHeight);
-
-
+   //Function to calculate angle between player and mouse
+   function calculateAngleToMouse(playerX, playerY) {
+      return Math.atan2(mouseY - playerY, mouseX - playerX) - Math.PI / 2; // Adjust for top-facing image
    }
 }
 
