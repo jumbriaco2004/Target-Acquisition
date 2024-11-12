9aafad1b33ca437feb6b5d0208971f5a0061b162
Gav1n197 <gavincorder2003@gmail.com>
2024-11-12 15:18:08 -0500

    Object-ified code and smaller player image file
    I made a smaller sprite of the player for testing purposes.
    Made the project more object oriented.

--- TargetAcquisition.js
+++ TargetAcquisition.js
@@ -2,37 +2,53 @@
 all files. This file should be like the initializeImages and startGame()
 functions in snailBait, super concise, not too much code.
 */
-
-var canvas = document.getElementById('game-canvas'),
-context = canvas.getContext('2d'),
-
-background = new Image(), //background image of canvas, not the webpage. The webpage background is done in the CSS file
-playerImage = new Image();
-
-initializeImages();
-
-function debug(stringText) //updates the debug element with a value given
+var TargetAcquisition = function() //Constructor
 {
-   document.getElementById('debug').innerHTML = stringText;
+   this.canvas = document.getElementById('game-canvas'),
+   this.context = this.canvas.getContext('2d'),
+
+   this.spritesheet = new Image(),
+   this.background = new Image(), //background image of the canvas, not the webpage. The webpage background is done in the CSS file
+   this.playerImage = new Image();
 }
 
-function initializeImages() //pretty much ripped from snailbait
+
+
+TargetAcquisition.prototype =
 {
-   background.src = "images/background_sprite.png";
-   playerImage.src = "images/player_sprite.png";
+   debug: function(stringText) 
+   /* Updates the debug element with a value given, best for supervising quickly updated values into the game 
+      will only show the last called debug functions value */
+   { document.getElementById('debug').innerHTML = stringText; },
+   
+   
 
-   background.onload = function (e) 
-   { 
-      drawGame();
-      //debug("yes");
-   };
-
-   function drawGame()
+   initializeImages: function ()
+   /* Sets the background and the images to certain urls in order to initialize the value of every image used
+      If any images are added, they should probably be initialized here */
    {
-      context.drawImage(background, 0, 0); //background drawn
-      //draw level here
-      context.drawImage(playerImage, 20, 520);
-      //draw other stuff here
-   }
-}
+      this.spritesheet.src = "images/SpriteSheet.png";
+      this.background.src = "images/background_sprite.png";
+      this.playerImage.src = "images/player_sprite.png";
 
+      this.background.onload = function (e) //after everything is defined
+      {
+         targetAcquisition.debug("Background loaded");
+         targetAcquisition.drawGame();
+      }
+   },
+
+   drawGame: function ()
+   /* Draws the game every frame, my need updated to have a drawSprites or Draw(whatever) sub-functions later */
+   {
+      this.debug("drawGame Called");
+      this.context.drawImage(this.background, 0, 0); //background drawn
+      //draw level here
+      this.context.drawImage(this.playerImage, 20, 520);
+      //draw other stuff here
+   },
+}  
+
+var targetAcquisition = new TargetAcquisition();
+
+targetAcquisition.initializeImages();
