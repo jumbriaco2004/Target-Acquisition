eec9270fa8b85b2597a41f601511110251785f94
Gav1n197 <gavincorder2003@gmail.com>
2024-11-22 18:59:05 -0500

    Beginnings of collision detection

--- spriteData.js
+++ spriteData.js
@@ -1,10 +1,15 @@
-/*This file is meant to contain all of the data about every sprite other than the player */
+/*  
+    This file is meant to contain all of the data about every sprite other than the player
+    The walls, projectile types, and other objects in the game should have their data here
+*/
 var SpriteData = function()
 {
+    this.debugGridOption = false;
     this.wallThick = 15; // Wall thickness
 
     this.walls_1 = // Level 1's walls
     [
+        {x: 0,    y: 200},
         {x: 0,    y: 200},  {x: 500,  y: 200}, 
         {x: 500,  y: 500},  {x: 1000, y: 500}, 
         {x: 1000, y: 100},  {x: 1400, y: 100}, 
@@ -33,19 +38,49 @@ SpriteData.prototype =
         }
     },
 
+    didCollide: function(x1, y1, x2, y2)
+    {
+        x1 = x1;
+        y1 = y1;
+        x2 = x2;
+        y2 = y2;
+        w1 = x2 - x1;
+        h1 = y2 - y1;
+        context.lineWidth = 1;
+        if ((w1 !== 0 && h1 !== 0)) //different code needs written for this part
+        {
+            
+            //context.fillStyle = "yellow"; 
+            //context.fillRect(x1, y1, w1, h1);
+        }
+        else // This part should work though
+        {
+
+            //context.fillStyle = "yellow";
+            //context.fillRect(x1, y1, w1, h1);
+        } 
+    },
+
     drawWalls: function(levelNum) //Draws a wall, and defines the wall as a sprite
     {
         context.save();
         context.beginPath();
-        context.strokeStyle = "black";
-        context.lineWidth = this.wallThick;
-        if (levelNum === 1)
+        //context.strokeStyle = "black";
+        //context.lineWidth = this.wallThick;
+        if (levelNum == 1)
         {
-            for (var i=0; i < this.walls_1.length; ++i) 
+            for (var i=1; i < this.walls_1.length; ++i) 
             {
+                this.didCollide(
+                    this.walls_1[(i - 1)].x, 
+                    this.walls_1[(i - 1)].y, 
+                    this.walls_1[i].x, 
+                    this.walls_1[i].y);
+                context.lineWidth = this.wallThick - 10;
                 context.lineTo(this.walls_1[i].x, this.walls_1[i].y);
                 context.stroke();
             }
+            
         }
         else if (levelNum === 2)
         {
@@ -55,7 +90,9 @@ SpriteData.prototype =
                     context.stroke();
                 }
         }
-        this.drawDebugGrid("red");
+        if (this.debugGridOption == true)
+            { this.drawDebugGrid("red"); }
+        
         context.restore();
     },
 
